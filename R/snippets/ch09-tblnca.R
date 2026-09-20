56a3a78bffcb0ad708409af0a4dbf2b5c5bfadeb
Kyun-Seop Bae <ksbae@acp.kr>
2026-09-21 08:40:11 +0900

    본문 저장소에서 다시 맞춘다 (2026-09-21)
    PKwR 의 R/snippets, output, figures, R/_freeze.R 을 그대로 옮겼다. 98개 파일.
    
    바뀐 내용은 둘이다.
    
      - Theoph 용량을 320 mg 으로 고친 것(ch09-tblnca.R, ch11-data.R). 자료의
        Dose 열은 mg/kg 이고 9번 대상자는 오기가 있어, 개인별 실제 용량인
        320 mg 을 쓴다. 11장의 출력 13개가 따라 바뀌었다.
      - _freeze.R 이 그림을 pdfcrop 으로 잘라 내게 된 것(2026-09-20). 그림
        82개가 잘린 판으로 바뀌었다.
    
    파일이 늘거나 줄지는 않았다. check.R 과 stamp.R 은 미러에 두지 않는 그대로다.

--- R/snippets/ch09-tblnca.R
+++ R/snippets/ch09-tblnca.R
@@ -1,7 +1,8 @@
-# 실측 자료: R 내장 Theoph (theophylline 경구, 12명)
-dw <- unique(Theoph[, c("Subject", "Wt", "Dose")])       # Dose 는 mg/kg
+# 실측 자료: R 내장 Theoph (theophylline 경구 320 mg 단회, 12명)
+# Theoph 의 Dose 열(mg/kg)은 320 mg 에서 파생된 값이라 되곱하면 반올림 오차가
+# 따라오고 9번에는 오기까지 있다. 아는 값 320 을 그대로 쓴다.
 tb <- tblNCA(Theoph, key = "Subject", colTime = "Time", colConc = "conc",
-             dose = dw$Dose*dw$Wt, adm = "Extravascular", concUnit = "mg/L")
+             dose = 320, adm = "Extravascular", concUnit = "mg/L")
 tb[, c("Subject", "CMAX", "TMAX", "LAMZHL", "AUCIFO", "AUCPEO", "CLFO")]
 
 # 요약: 기하평균과 기하 CV% (15장의 노출 요약 관례)
--- R/snippets/ch11-data.R
+++ R/snippets/ch11-data.R
@@ -1,7 +1,9 @@
 # Theoph 를 NONMEM 형식으로: ID, TIME, DV 와 용량(DOSE, mg), 공변량(BWT)
+# 이 시험은 12명 모두 320 mg 단일 경구 투여다. Theoph 의 Dose 열(mg/kg)은
+# 거기서 파생된 값이라 반올림 오차를 싣고 9번에는 오기가 있으므로 쓰지 않는다.
 DATA <- data.frame(ID   = as.numeric(as.character(Theoph$Subject)),
                    TIME = Theoph$Time, DV = Theoph$conc,
-                   BWT  = Theoph$Wt,   DOSE = round(Theoph$Dose*Theoph$Wt, 1))
+                   BWT  = Theoph$Wt,   DOSE = 320)
 DATA <- DATA[order(DATA$ID, DATA$TIME), ]     # ID 오름차순 정렬 [필수]
 stopifnot(!is.unsorted(DATA$ID))              # 정렬 assertion (저장 직전 검증)
 head(DATA, 3)
